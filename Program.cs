4008d1ed296e5c3c18dc694b19ed2b96533f2dc4
fc <fc@DESKTOP-1R1FJCU>
2025-10-07 19:01:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using szineszek;
+namespace szineszek
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fajl = "D:\\Info\\random\\szineszek\\szineszek.txt";
+
+            List<Szineszek> objektum = new List<Szineszek>();
+
+            using (StreamReader olvaso = new StreamReader(fajl))
+            {
+                //_ = olvaso.ReadLine(); első sor átugrása
+                
+
+                while (!olvaso.EndOfStream)
+                {
+                    objektum.Add(new(olvaso.ReadLine()));
+                }
+
+                foreach (var item in objektum)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+
+            Console.WriteLine($"A sorok száma:{objektum.Count}");
+
+        }
+    }
+
+}
---
+++ szineszek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szineszek
+{
+    class Szineszek
+    {
+        public string SzineszNeve { get; set; }
+        public string SzineszNeme { get; set; }
+        public string SzulHely { get; set; }
+        public string SzulOrszag { get; set; }
+        public int FilmSzam { get; set; }
+
+        public Szineszek(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            SzineszNeve = adatok[0];
+            SzineszNeme = adatok[1];
+            SzulHely = adatok[2];
+            SzulOrszag = adatok[3];
+            FilmSzam = int.Parse(adatok[4]);
+        }
+
+
+        public override string ToString()
+        {
+            return $"Szinesz neve:{SzineszNeve}\n Szinész neme:{SzineszNeme}\n Születési helye:{SzulHely} \n Szulorszag: {SzulOrszag} \n FilmSzam: {FilmSzam}";
+        }
+    }
+}
